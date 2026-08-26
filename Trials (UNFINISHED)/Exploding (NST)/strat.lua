551c8461e369ab0c2758a12a4075dd1b142d9747
Dosia124 <70428357+Dosia124@users.noreply.github.com>
2026-08-26 15:36:14 +0500

    Update strat.lua

--- Trials (UNFINISHED)/Exploding (NST)/strat.lua
+++ Trials (UNFINISHED)/Exploding (NST)/strat.lua
@@ -1,3 +1,4 @@
+getgenv().AutoDJ = true; getgenv().AutoChain = true; getgenv().AutoSupportCaravan = true;
 local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
 
 TDS:Loadout("Cowboy", "Tesla", "Minigunner", "DJ Booth", "Commander")
