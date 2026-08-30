c7bc2342cfd613c95a7949bb77ca2ceb21e1b67e
Dosia124 <70428357+Dosia124@users.noreply.github.com>
2026-08-30 20:40:41 +0500

    reworked ALL evolved xp strats dude omg bruh

--- Evolved XP/2 Normal Towers/Minigunner/Minigunner-Scout.lua
+++ Evolved XP/2 Normal Towers/Minigunner/Minigunner-Scout.lua
@@ -1,50 +1,39 @@
-getgenv().AntiLag = true; getgenv().AutoRejoin = true; getgenv().AutoRestart = false
-
+getgenv().AntiLag = true; getgenv().AutoMercenary = true; getgenv().MaxPathDistance = 300; getgenv().AutoRejoin = true; getgenv().AutoRestart = false
 local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
-TDS:Loadout("Trapper", "Hacker", "Gatling Gun", "Minigunner", "Scout")
+
+TDS:Loadout("Trapper", "Hacker", "Gatling Gun", "Scout", "Minigunner")
 TDS:Mode("Fallen")
-TDS:GameInfo("Summer Castle", {HiddenEnemies = true, Glass = true, ExplodingEnemies = true, Limitation = true, Committed = true, Quarantine = true, Fog = true, SpeedyEnemies = true, Broke = true})
+TDS:GameInfo("Lay By", {HiddenEnemies = true, Glass = true, SpeedyEnemies = true, Limitation = true, Committed = true, Fog = true, ExplodingEnemies = true, Quarantine = true})
 
-TDS:VoteSkip(11, 39)
-TDS:Place("Trapper", 16.109100341796875, 0.9744765758514404, 18.9939022064209) --1
+TDS:VoteSkip(8, 39)
+TDS:Place("Trapper", -2.6489198207855225, 243, 176)
 TDS:Ready()
-TDS:Place("Trapper", 10.98687744140625, 0.968543529510498, 20.330904006958008) --2
-TDS:Upgrade(2)
+TDS:Place("Trapper", 2.570160150527954, 243, 175.9499969482422)
 TDS:Upgrade(1)
-TDS:Place("Trapper", 16.051319122314453, 0.9414479732513428, 15.954482078552246) --3
+TDS:Upgrade(2)
+TDS:Place("Gatling Gun", 0.011896941810846329, 243, 149.94903564453125)
 TDS:Upgrade(3)
-TDS:Place("Trapper", 19.128238677978516, 0.9785783290863037, 18.971010208129883) --4
+TDS:Upgrade(3)
+TDS:Upgrade(3)
+TDS:Place("Hacker", 2.583040237426758, 242.99998474121094, 266.0757751464844)
 TDS:Upgrade(4)
-TDS:Place("Trapper", 21.974578857421875, 0.981574296951294, 17.847187042236328) --5
+TDS:Upgrade(4)
+TDS:Place("Hacker", -2.662294864654541, 242.99998474121094, 282.9828186035156)
 TDS:Upgrade(5)
-TDS:Place("Trapper", 22.9644775390625, 0.9752120971679688, 14.970300674438477) --6
-TDS:Upgrade(6)
-TDS:Place("Trapper", 10.860502243041992, 0.9707396030426025, 23.329011917114258) --7
-TDS:Upgrade(7)
-TDS:VoteSkip(10)
-TDS:Place("Gatling Gun", -27.217533111572266, 0.9903836250305176, -31.249570846557617) --8
-TDS:Upgrade(8)
-TDS:Upgrade(8)
-TDS:Upgrade(8)
-TDS:Upgrade(8)
-TDS:Place("Hacker", 6.097320556640625, 0.9701399803161621, 30.797021865844727) --9
-TDS:Upgrade(9)
-TDS:Upgrade(9)
-TDS:Place("Hacker", 22.965129852294922, 0.9872190952301025, 28.965984344482422) --10
-TDS:Upgrade(10)
-TDS:Upgrade(10)
-TDS:Upgrade(9)
-TDS:Upgrade(9)
-TDS:Upgrade(10)
-TDS:Upgrade(10)
-TDS:Ability(9, "Hologram Tower", {towerPosition = Vector3.new(-27.22711753845215, 0.9885187149047852, -26.59650230407715), towerToClone = 8}, true)
-TDS:Ability(10, "Hologram Tower", {towerPosition = Vector3.new(-27.22711753845215, 0.9885187149047852, -26.59650230407715), towerToClone = 8}, true)
-TDS:Upgrade(8)
-TDS:Upgrade(8)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:Ability(5, "Hologram Tower", {towerPosition = Vector3.new(0.06768691539764404, 243, 145.91421508789062), towerToClone = 3}, true)
+TDS:Upgrade(4)
+TDS:Upgrade(4)
+TDS:Ability(4, "Hologram Tower", {towerPosition = Vector3.new(0.06768691539764404, 243, 145.91421508789062), towerToClone = 3}, true)
+TDS:Upgrade(3)
+TDS:Upgrade(3)
+TDS:Upgrade(3)
 TDS:WaitForWave(38)
-TDS:Upgrade(9, 2)
-TDS:Upgrade(10, 2)
+TDS:Upgrade(5, 2)
+TDS:Upgrade(4, 2)
 
-TDS:Place("Scout", 17.945087432861328, 0.9868543148040771, 33.873382568359375)
+TDS:Place("Scout", 4.557474613189697, 243, 149.94903564453125)
 
-TDS:Place("Minigunner", 15.779468536376953, 0.9804263114929199, 27.094398498535156)
+TDS:Place("Minigunner", 2.6362199783325195, 243, 155.05084228515625)
